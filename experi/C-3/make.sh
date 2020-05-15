ef8899a371c14f467566fed18ebc9be80d252c8a
RyosukeSasaki <rsasaki@keio.jp>
2020-05-15 15:46:28 +0900

    修正

--- experi/C-3/make.sh
+++ experi/C-3/make.sh
@@ -1,1 +1,1 @@
-pdftk C-3佐々木良輔_.pdf out/main.pdf 1-1.pdf 1-2.pdf 2-1.pdf 2-2.pdf note.pdf kami.pdf cat output C-3佐々木良輔.pdf
+pdftk C-3佐々木良輔_.pdf out/main.pdf 1-1.pdf 1-2.pdf 2-1.pdf 2-2.pdf note.pdf kami.pdf cat output C-3佐々木良輔R1.pdf
